58ed0d491e8a621e7c288adab2c74366a16476e2
Joshua Martin <josmar52789@gmail.com>
2011-07-23 12:33:11 -0400

    Added support for dragging files outside of Median.

--- Median/CFilesTableView.m
+++ Median/CFilesTableView.m
@@ -14,13 +14,17 @@
 
 - (void)awakeFromNib
 {
-    // accept only file drags and set datasource delegate
-    [self registerForDraggedTypes:[NSArray arrayWithObject:NSFilenamesPboardType]];
+    // accept only file drags and set datasource delegate and table delegate
+    [self registerForDraggedTypes:[NSArray arrayWithObjects:NSFilenamesPboardType, NSFilesPromisePboardType, nil]];
     [self setDataSource:self];
-    
     [self setDelegate:self];
     
+    // double clicks should open the most recent version of the selected file
     [self setDoubleAction:@selector(openMostRecentVersion:)];
+    
+    // set the allowed drag source operations
+    [self setDraggingSourceOperationMask:NSDragOperationNone forLocal:YES]; //local
+    [self setDraggingSourceOperationMask:NSDragOperationCopy forLocal:NO]; // external
 }
 
 /**
@@ -32,11 +36,9 @@
 }
 
 /**
-    Delegate for drag and drop, performs the actual operation when the drag is
-    accepted. Collects the list of filenames to be moved, and initiates the move
-    with the CFileController.
+    Delegate for drag and drop, performs the actual operation when the drag is accepted. Collects the list of filenames to be moved, and initiates the move with the CFileController.
  */
-- (BOOL)tableView:(NSTableView *)aTableView acceptDrop:(id <NSDraggingInfo>)info
+- (BOOL)tableView:(NSTableView *)tableView acceptDrop:(id <NSDraggingInfo>)info
               row:(NSInteger)row dropOperation:(NSTableViewDropOperation)operation
 {
     NSPasteboard* pboard = [info draggingPasteboard];
@@ -49,6 +51,76 @@
     return YES;
 }
 
+/**
+    Delegate for drag and drop that returns whether or not a drag and drop operation should begin and sets the data to the pasteboard.
+ */
+- (BOOL)tableView:(NSTableView *)tableView writeRowsWithIndexes:(NSIndexSet *)rowIndexes toPasteboard:(NSPasteboard*)pboard
+{
+    // set the pasteboard for HFS promises only
+    [pboard declareTypes:[NSArray arrayWithObjects:NSFilesPromisePboardType, nil] owner:self];
+    
+    // the pasteboard must know the type of files being promised
+    NSMutableArray *filenameExtensions = [NSMutableArray array];
+    
+    // iterate the selected files and get the extension from each file
+    NSArray * selectedObjects = [filesController selectedObjects];
+    for (File *o in selectedObjects) {
+        NSString *filename = [o valueForKey:@"filename"];
+        
+        NSString *filenameExtension = [filename pathExtension];
+        if (![filenameExtension isEqualToString:@""]) {
+            [filenameExtensions addObject:filenameExtension];
+        }
+    }
+    
+    // give the pasteboard the file extensions
+    [pboard setPropertyList:filenameExtensions
+                    forType:NSFilesPromisePboardType];
+    
+    return YES;
+}
+
+/**
+    Delegate for drag and drop returns an array of the filenames.
+ */
+- (NSArray *)tableView:(NSTableView *)aTableView namesOfPromisedFilesDroppedAtDestination:(NSURL *)dropDestination forDraggedRowsWithIndexes:(NSIndexSet *)indexSet
+{
+    // return of the array of file names
+    NSMutableArray *draggedFilenames = [NSMutableArray array];
+    
+    // iterate the selected files
+    NSArray * selectedObjects = [filesController selectedObjects];
+    for (File *f in selectedObjects) {
+        [draggedFilenames addObject:[f valueForKey:@"filename"]];
+        
+        // the file's pretty filename (i.e. filename.txt)
+        NSString *fFilename = [f valueForKey:@"filename"];
+        // the file's most recent version's unique id
+        NSString *vFilename = [[f mostRecentVersion] valueForKey:@"filename"];
+        
+        NSString *fullPathToOriginal = [NSString stringWithFormat:@"%@/%@", [[appDelegate dataDirectory] path], vFilename];
+        NSString *destPath = [[dropDestination path] stringByAppendingPathComponent:fFilename];
+        
+        // if a file with the same name exists on the destination, append " - Copy" to the filename
+        BOOL fileExists = [[NSFileManager defaultManager] fileExistsAtPath:destPath];
+        
+        if (fileExists)
+        {
+            fFilename = [NSString stringWithFormat:@"%@ - Copy.%@", [[fFilename lastPathComponent] stringByDeletingPathExtension],[fFilename pathExtension]];
+        }
+        
+        // perform the actual copy
+        CDataController *dataController = [CDataController alloc];
+        [dataController performTransferFromPath:fullPathToOriginal destinationDirectory:[dropDestination path] destinationFilename:fFilename];
+        [dataController release];
+    }
+    
+    return draggedFilenames;
+}
+
+/**
+    Handle delete key presses by removing the selected files.
+ */
 - (void)keyDown:(NSEvent*)event
 {
     BOOL deleteKeyEvent = NO;
